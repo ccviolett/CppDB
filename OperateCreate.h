b2edee2b26c895e1d7c02eb9dd99128081d24c35
Carmine Carlo <3340751717@qq.com>
2023-09-20 12:18:48 +0800

    :sparkles: feat(OperateCreate): Create OperateCreate.h to determine CREATE command

---
+++ OperateCreate.h
@@ -0,0 +1,16 @@
+#ifndef _OPERATE_CREATE_H
+#define _OPERATE_CREATE_H
+
+#include "Operate.h"
+
+class OperateCreate : public Operate {
+	public:
+		OperateCreate(String s) {
+		}
+
+		static bool checkType(String s, String type = "CREATE") {
+			return Operate::checkType(s, type);
+		}
+};
+
+#endif
